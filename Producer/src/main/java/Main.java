d6ae0cba63be7839a81622d7494cd4c056f1926c
15611 <15611525238@163.com>
2024-04-22 18:13:04 +0100

    test the function of using MySQL to store the json object in the topic into the database.

--- Producer/src/main/java/Main.java
+++ Producer/src/main/java/Main.java
@@ -2,9 +2,11 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,14 +19,20 @@ public class Main {
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+
         // create a Kafka producer object
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        // create a JSON object
+        JSONObject data = new JSONObject();
+        Random random = new Random();
+        int id = random.nextInt(1000);
+        int id1 = random.nextInt(1000);
+        data.put("id", id1);
+        data.put("username", "sky");
+        data.put("goodsname", "package");
 
-        // send the records
-        for (int i = 0; i < 10; i++) {
-            ProducerRecord<String, String> record = new ProducerRecord<>("test-topic", "key" + i, "value" + i);
-            producer.send(record);
-        }
+        producer.send(new ProducerRecord<>("test-topic1", String.valueOf(id) , data.toString()));
+
 
         // close the producer
         producer.close();
