9e57f89a765439a552d593d34912192c09496b17
Xiaoyu Han <xiaoyu.han@ucdconnect.ie>
2024-04-27 21:46:23 +0100

    create kafka producer

--- consumer/main.py
+++ consumer/main.py
@@ -20,12 +20,15 @@ consumer = KafkaConsumer(topic,
 
 print("Connected to Kafka server")
 
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
 # 消费消息
 try:
     for message in consumer:
         json_message = json.loads(message.value.decode('utf-8'))
         sentiment = classifier.predict(json_message['review'])
         print(f"Predict result of review_{json_message['id']} is {sentiment}")
+        producer.send('result_topic', json.dumps({'id': json_message['id'], 'movie_id': json_message['movie_id'] ,'sentiment': int(sentiment)}).encode('utf-8'))
 except KeyboardInterrupt:
     # 在接收到键盘中断（Ctrl+C）时关闭消费者连接
     consumer.close()
